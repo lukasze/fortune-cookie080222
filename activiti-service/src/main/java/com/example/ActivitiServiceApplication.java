9720758287ba2d02667e083a934db127237828e9
KazmierczaBa <bartlomiej.kazmierczak1@fujitsu.com>
2022-02-09 10:24:20 +0100

    activiti service implementation

---
+++ activiti-service/src/main/java/com/example/ActivitiServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiServiceApplication.class, args);
+    }
+}
--- activiti-service/src/main/java/com/example/controller/ActivityController.java
+++ activiti-service/src/main/java/com/example/controller/ActivityController.java
@@ -1,4 +1,29 @@
 package com.example.controller;
 
+
+import com.example.Message;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
 public class ActivityController {
+
+    @Value("${activities}")
+    private String[] activities;
+
+    @Value("${spring.application.name}")
+    private String serviceName;
+
+    @GetMapping("/activity")
+    public Message getActivity() {
+        return getMsgWithRandomActivity();
+    }
+
+    private Message getMsgWithRandomActivity() {
+        var msg = activities[new Random().nextInt(activities.length)];
+        return new Message(serviceName.toUpperCase(), msg);
+    }
 }
